521ad844d3d7d29b78de2a90d7750ec66e1e96b2
rifatmahmud <rftmhmd@gmail.com>
2013-08-04 02:38:45 +0600

    The header for matching and outputting characters

---
+++ match.h
@@ -0,0 +1,20 @@
+#ifndef MATCH_H_INCLUDED
+#define MATCH_H_INCLUDED
+
+#include <cv.h>
+#include <highgui.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+namespace cmp
+{
+
+
+    void creat_char_list(std::vector<std::vector<char> > &out_letters);
+    void mtch_cnt(cv::vector<cv::vector<cv::Point> > input, cv::vector<cv::vector<cv::Point> > rosetta, std::vector<std::vector<char> > letters);
+}
+
+
+
+#endif // MATCH_H_INCLUDED
