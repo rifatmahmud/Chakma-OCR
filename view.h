288237d2bcbdf8c4a6108aec26d81400d28338a2
rifatmahmud <rftmhmd@gmail.com>
2013-08-04 02:21:51 +0600

    The header for writing and viewing image

---
+++ view.h
@@ -0,0 +1,17 @@
+#ifndef VIEW_H_INCLUDED
+#define VIEW_H_INCLUDED
+
+#include <cv.h>
+#include <highgui.h>
+
+namespace view
+{
+
+    void show_image(cv::Mat image);
+    void write_image(cv::Mat image, const char *filepath);
+
+}
+
+
+
+#endif // VIEW_H_INCLUDED
