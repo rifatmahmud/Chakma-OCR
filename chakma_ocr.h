d64869c636e29f54c66d6f1bddfa1f4593850945
rifatmahmud <rftmhmd@gmail.com>
2013-08-04 01:51:11 +0600

    The abstracted header

---
+++ chakma_ocr.h
@@ -0,0 +1,11 @@
+#ifndef CHAKMA_OCR_H_INCLUDED
+#define CHAKMA_OCR_H_INCLUDED
+
+#include "image.h"
+#include "view.h"
+#include "contour.h"
+#include "match.h"
+
+
+
+#endif // CHAKMA_OCR_H_INCLUDED
