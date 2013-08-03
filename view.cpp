2474f7144985d778dff051278feede503ec2a92b
rifatmahmud <rftmhmd@gmail.com>
2013-08-04 02:23:30 +0600

    The header implementation for writing and viewing image

---
+++ view.cpp
@@ -0,0 +1,21 @@
+#include <cv.h>
+#include "view.h"
+#include <highgui.h>
+
+namespace view
+{
+
+    void show_image(cv::Mat image)
+    {
+        cv::namedWindow(" ", 1);
+        cv::imshow(" ", image);
+        cv::waitKey(0);
+
+    }
+
+    void write_image(cv::Mat image, const char *filepath)
+    {
+        cv::imwrite(filepath, image);
+    }
+
+}
